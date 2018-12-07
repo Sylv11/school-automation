5ef55a0bd5c9bc8f2643e7da08e53e8b374453fc
Sylv11 <sylvain.urbain.isf@gmail.com>
2018-12-07 16:55:50 +0100

    Dashboard action bar

--- app/src/main/java/com/example/sylvain/projetautomates/Activity/DashboardActivity.java
+++ app/src/main/java/com/example/sylvain/projetautomates/Activity/DashboardActivity.java
@@ -1,12 +1,14 @@
 package com.example.sylvain.projetautomates.Activity;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
 
 import com.example.sylvain.projetautomates.DB.User;
 import com.example.sylvain.projetautomates.Network;
@@ -14,12 +16,11 @@ import com.example.sylvain.projetautomates.R;
 import com.example.sylvain.projetautomates.Session;
 import com.example.sylvain.projetautomates.ToastService;
 
-import java.util.Timer;
-
 public class DashboardActivity extends AppCompatActivity {
 
     private TextView tv_dashboard_welcome;
-    private Button btn_dashboard_logout;
+    //private Button btn_dashboard_logout;
+    private Toolbar toolbar = null;
 
     private Session session;
     private Network network;
@@ -28,8 +29,14 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        this.toolbar = findViewById(R.id.toolbar);
+        this.setSupportActionBar(this.toolbar);
+        this.getLayoutInflater().inflate(R.layout.action_bar, null);
+
+
         tv_dashboard_welcome = (TextView)findViewById(R.id.tv_dashboard_welcome);
-        btn_dashboard_logout = (Button)findViewById(R.id.btn_dashboard_logout);
+       // btn_dashboard_logout = (Button)findViewById(R.id.btn_dashboard_logout);
 
         session = new Session(this);
         network = new Network(this);
@@ -48,11 +55,34 @@ public class DashboardActivity extends AppCompatActivity {
     public void onDashboardClickManager(View v) {
         switch(v.getId()){
 
-            case R.id.btn_dashboard_logout:
-                session.closeSession();
+            //case R.id.btn_dashboard_logout:
+                //session.closeSession();
 
-                ToastService.show(this, "Déconnecté");
-                break;
+                //ToastService.show(this, "Déconnecté");
+               // break;
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_dashboard_logout :
+                this.session.closeSession();
+                ToastService.show(this,"Déconnecté");
+                break;
+        }
+
+        return true;
+    }
+
+    public void openMenu(View view) {
+        this.toolbar.showOverflowMenu();
+    }
+
 }
